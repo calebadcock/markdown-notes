c19a9fe8c7fd89f5077ddc5ac72abe29e2e0be12
Eric Groh <ericgroh@Erics-MacBook-Pro.local>
2018-11-28 13:36:28 -0500

    Added navbar component and added markdown preview from text area

--- app/containers/Notes.jsx
+++ app/containers/Notes.jsx
@@ -3,24 +3,42 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classNames from 'classnames/bind';
 import styles from '../css/containers/notes.css';
+import Navbar from '../components/Navbar.jsx';
+import ReactMarkdown from 'react-markdown';
 
 const cx = classNames.bind(styles);
 /**
  * Container for the notes
  */
 class Notes extends Component {
+    /**
+     * Sets the initial state of the Notes page
+     * @param {Object} props
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: `# Welcome!`
+        };
+    }
 
-  /**
-   * Renders notes container
-   * @return {*}
-   */
-  render() {
-    return (
-      <div>
-        Notes Here
-      </div>
-    );
-  }
+    handleChange = (e) => {
+        this.setState({ text: e.target.value });
+    };
+
+    /**
+    * Renders notes container
+    * @return {*}
+    */
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <textarea onChange={this.handleChange} value={this.state.text}></textarea>
+                <ReactMarkdown source={this.state.text} />
+            </div>
+        );
+    }
 }
 
 Notes.propTypes = {
