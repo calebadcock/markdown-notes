da09abe945053fc3b238b5625cc5b547abadba2d
Eric Groh <ericgroh@Erics-MacBook-Pro.local>
2018-11-28 13:09:22 -0500

    added markdown converter and connected it to textare

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+
+// import styles from '../css/containers/navbar.css';
+
+/**
+* Navbar component
+*
+*/
+class Navbar extends Component {
+    /**
+    * @return {*}
+    *
+    */
+    render() {
+        return (
+            <div>
+                Navbar
+            </div>
+        );
+    }
+}
+
+export default Navbar;
