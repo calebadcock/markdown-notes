d652dd1ee4af1b750752abee1ee7c7b15d495d60
ericgroh <grohec.1995@gmail.com>
2018-11-28 23:05:42 -0500

    Added navbar component and added markdown preview from text area (#1)

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+// import styles from '../css/containers/navbar.css';
+
+/**
+* Navbar component
+*
+*/
+class Navbar extends Component {
+    /**
+    * @return {*}
+    *
+    */
+    render() {
+        return (
+            <div>
+                Navbar
+            </div>
+        );
+    }
+}
+
+export default Navbar;
